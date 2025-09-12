aa3222f2ff1bbfeb1b25e3024551dd78a16a1415
souzaa22 <matheus.l.barros7@aluno.senai.br>
2025-09-12 16:13:35 -0300

    Add files via upload

---
+++ Lista11whilee.java
@@ -0,0 +1,22 @@
+package primeroprojeto;
+
+import java.util.Scanner;
+
+public class Lista11whilee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   Scanner sc = new Scanner(System.in);
+		 //1
+	        int numero =1;
+	        while (numero<= 31) {
+	        	System.out.println(numero);
+	        	numero+= 2;
+	        }
+		   
+		   
+		   
+		   
+	}
+
+}
