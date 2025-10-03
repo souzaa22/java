ee03bfe200f2a9127c37b3961288d2f78dccaa99
souzaa22 <matheus.l.barros7@aluno.senai.br>
2025-10-03 15:23:55 -0300

    Add files via upload

---
+++ Lista18.java
@@ -0,0 +1,122 @@
+package primeroprojeto;
+
+
+public class Lista18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		mensagem();
+		mensagem();
+		mensagem();
+		mensagem();
+		separador();
+		MensagemMotivacional();
+		separador();
+		contagem();
+		separador();
+		vogal();
+		separador();
+		quadrado();
+		separador();
+		pares();
+		separador();
+		regresso();
+		separador();
+		triangulo();
+		separador();
+		diadesemana();
+		
+		
+		
+		
+	}
+	//1
+	public static void mensagem() {
+			System.out.println("Matheus");
+		}
+		public static void separador() {
+			System.out.println("------------------------------------");
+		}
+         //2                                                                                                                                                                                                                              
+		public static void MensagemMotivacional(){
+			System.out.println("Mensagem motivacional");
+		}
+		
+		//3
+		
+		public static  void contagem() {
+			for(int i = 1; i <=10; i++) {
+				System.out.println(i);
+			}
+		}
+		//4
+		public static  void vogal() {
+			for(int i = 1; i <=1; i++) {
+				System.out.println("A-E-I-O-U");
+		}
+		}
+
+		//5
+		public static void quadrado() {
+			System.out.println("|**|");
+			System.out.println("|**|");
+			System.out.println("--------------------------------");
+	}
+				
+			
+			//6
+			
+			public static  void pares() {
+				for(int i = 0; i<=20; i+=2) {
+			
+					System.out.println(i);}}
+		
+			//7
+					public static void regresso() {
+						for(int i = 10; i >=1; i--) {
+							System.out.println(i);
+							}
+						System.out.println("Fim");}
+			
+
+					// 8
+					public static void triangulo() {
+								System.out.println(" *");
+								System.out.println(" **");
+								System.out.println(" *");
+								System.out.println(" **");
+								System.out.println(" ***");
+					}
+					{
+									System.out.println("--------------------------------");
+							}
+			
+			//9
+					public static void diadesemana() {
+System.out.println("Segunda");
+System.out.println("Terça");
+System.out.println("Quarta");
+System.out.println("Quinta");
+System.out.println("Sexta");
+System.out.println("Sábado");
+System.out.println("Domingo");
+
+
+
+
+
+					}
+					
+					
+					
+}
+		
+		
+		
+		
+		
+		
+		
+	
+
+
